1c656e4b2788f2a780f3ee5924ad5164c4ff29e8
Sarah Elzinga <sevans924@gmail.com>
2020-08-05 11:16:09 -0400

    added scope chain jpg file to img

--- part-2-scope-chain/review.js
+++ part-2-scope-chain/review.js
@@ -1,2 +1,23 @@
 
 //create diagram demonstrating scope chain
+
+let a = 'I am global'
+
+function soManyVariables(arg1){
+    let c = 23
+    let bubble1 = function(arg2){
+        let dog = 'I belong to level1'
+        function bubble2(parameter){
+            let x = 137
+            let bubble3 = function(param1){
+                let message = 'I really need to work on naming my variables'
+            }
+            return bubble3
+        }
+        return bubble2
+    }
+return bubble1
+}
+
+
+let b = 'I am also global'
