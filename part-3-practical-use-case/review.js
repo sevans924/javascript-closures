44fde1863150e42a21599b08337ac6dfb4facecd
sevans924 <43916517+sevans924@users.noreply.github.com>
2020-08-05 15:16:07 -0400

    Update review.js

--- part-3-practical-use-case/review.js
+++ part-3-practical-use-case/review.js
@@ -1,8 +1,4 @@
 
-//walk through student activity
-//discuss use case
-//discuss how this relates to objects and constructors
-//inheritance will come again when learning React
 
 //1. Build out the empty counter function below to create a counter and a closure that can operate on the counter(increment, decrement, add 5, etc.).
 //2. Define 2 variables and assign them the value of your counter function so that each variable has a different value when invoked(just like the example that you completed with your instructor).
